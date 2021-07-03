5eae4277919de32455eec788d4003a4c2a8fcbc1
Saksham Chaturvedi <81289215+saksshham@users.noreply.github.com>
2021-07-03 06:51:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const irony = document.querySelector('.b');
+irony.addEventListener('click', (e) =>{
+    alert("Here, I call myself as a Frontend Web Developer, however, the website isn't responsive + the code could've been way better written. Hence, the irony :')")
+})
